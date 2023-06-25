3d4fa7b3629d6a0d43447cb8f5e55a776106d7d6
Ramon Melo <ramonmelo@ramonmelo.com>
2023-06-25 11:11:12 -0300

    Changed Player Collision Checks

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 
@@ -19,10 +20,45 @@ public class Player : MonoBehaviour {
 
     // Move
     var moveDir = new Vector3(inputDir.x, 0, inputDir.y);
-    transform.position += moveDir * moveSpeed * Time.deltaTime;
-    transform.forward = Vector3.Slerp(transform.forward, moveDir, rotateSpeed * Time.deltaTime);
 
-    // State
+    // Collision Check
+    var moveDistance = moveSpeed * Time.deltaTime;
+    var canMove = CheckMove(moveDir, moveDistance);
+
+    if (canMove == false) {
+      var moveDirX = new Vector3(inputDir.x, 0, 0);
+      canMove = CheckMove(moveDirX, moveDistance);
+
+      if (canMove) {
+        moveDir = moveDirX;
+      }
+    }
+
+    if (canMove == false) {
+      var moveDirZ = new Vector3(0, 0, inputDir.y);
+      canMove = CheckMove(moveDirZ, moveDistance);
+
+      if (canMove) {
+        moveDir = moveDirZ;
+      }
+    }
+
+    moveDir = moveDir.normalized;
+
+    if (canMove) {
+      transform.position += moveDir * moveDistance;
+    } else {
+      moveDir = Vector3.zero;
+    }
+
     IsWalking = moveDir.sqrMagnitude > 0;
+
+    transform.forward = Vector3.Slerp(transform.forward, moveDir, rotateSpeed * Time.deltaTime);
+  }
+
+  private bool CheckMove(Vector3 moveDir, float moveDistance) {
+    float playerRadius = 0.7f;
+    float playerHeight = 2f;
+    return Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDir, moveDistance) == false;
   }
 }
